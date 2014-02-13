89aeea58baaeca1ec3b1d281a30e4859f1d750bc
Tom Anderson <tanderson@soliantconsulting.com>
2014-02-12 20:27:23 -0800

    Moved module to Admin

--- src/ZF/Apigility/Doctrine/Admin/Module.php
+++ src/ZF/Apigility/Doctrine/Admin/Module.php
@@ -44,7 +44,7 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/../../../../config/module.config.php';
+        return include __DIR__ . '/../../../../../config/module.config.php';
     }
 
     public function getServiceConfig()
