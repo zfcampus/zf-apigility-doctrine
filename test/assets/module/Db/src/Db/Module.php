7aaa4be79d99ca3b9816076a587277a97f278296
Tom Anderson <tanderson@soliantconsulting.com>
2014-03-21 14:16:14 -0700

    Tests copied from -admin

---
+++ test/assets/module/Db/src/Db/Module.php
@@ -0,0 +1,23 @@
+<?php
+namespace Db;
+
+use ZF\Apigility\Provider\ApigilityProviderInterface;
+
+class Module implements ApigilityProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+}
---
+++ test/assets/module/DbApi/config/module.config.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(),
+    ),
+    'zf-versioning' => array(
+        'uri' => array(),
+    ),
+    'zf-rest' => array(),
+    'zf-content-negotiation' => array(
+        'controllers' => array(),
+        'accept-whitelist' => array(),
+        'content-type-whitelist' => array(),
+        'accept_whitelist' => array(),
+        'content_type_whitelist' => array(),
+    ),
+    'zf-hal' => array(
+        'metadata_map' => array(),
+    ),
+    'zf-rest-doctrine-hydrator' => array(),
+    'zf-apigility' => array(
+        'doctrine-connected' => array(),
+    ),
+    'controllers' => array(
+        'invokables' => array(),
+    ),
+    'zf-rpc' => array(),
+    'zf-rpc-doctrine-controller' => array(),
+);
