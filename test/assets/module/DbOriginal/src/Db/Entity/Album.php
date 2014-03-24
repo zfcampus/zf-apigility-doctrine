7457eab277fc916738cf370cfb39ff03ef832864
Tom Anderson <tanderson@soliantconsulting.com>
2014-03-24 10:31:33 -0700

    Created reset for testing

---
+++ test/assets/module/DbOriginal/src/Db/Entity/Album.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Db\Entity;
+
+class Album
+{
+    protected $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    protected $name;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($value)
+    {
+        $this->name = $value;
+        return $this;
+    }
+
+    protected $createdAt;
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\Datetime $value)
+    {
+        $this->createdAt = $value;
+        return $this;
+    }
+
+    protected $artist;
+
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+}
