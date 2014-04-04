5fe740bfceadbc9d6b0b933c5e8f28da3411dc06
Tom Anderson <tanderson@soliantconsulting.com>
2014-04-04 13:53:42 -0700

    Added support for field math + normalized filter code to abstract

--- src/Server/Collection/Filter/ORM/Like.php
+++ src/Server/Collection/Filter/ORM/Like.php
@@ -6,18 +6,9 @@ class Like extends AbstractFilter
 {
     public function filter($queryBuilder, $metadata, $option)
     {
-        if (isset($option['where'])) {
-            if ($option['where'] == 'and') {
-                $queryType = 'andWhere';
-            } elseif ($option['where'] == 'or') {
-                $queryType = 'orWhere';
-            }
-        }
+        $queryType = $this->normalizeQueryType($option);
+        $field = $this->normalizeField($option['field'], $queryBuilder, $metadata);
 
-        if (!isset($queryType)) {
-            $queryType = 'andWhere';
-        }
-
-        $queryBuilder->$queryType($queryBuilder->expr()->like('row.' . $option['field'], $queryBuilder->expr()->literal($option['value'])));
+        $queryBuilder->$queryType($queryBuilder->expr()->like($field, $queryBuilder->expr()->literal($option['value'])));
     }
 }
