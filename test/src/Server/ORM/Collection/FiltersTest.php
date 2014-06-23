0683b33a71c3fa243b176b74738a16c3d464cc96
Tom Anderson <tanderson@soliantconsulting.com>
2014-06-23 10:17:01 -0700

    Fix namespace; remove Admin

--- test/src/Server/ORM/Collection/FiltersTest.php
+++ test/src/Server/ORM/Collection/FiltersTest.php
@@ -55,6 +55,7 @@ class FiltersTest extends \Zend\Test\PHPUnit\Controller\AbstractHttpControllerTe
 
         $this->dispatch("/test/artist?$queryString");
         $body = json_decode($this->getResponse()->getBody(), true);
+print_r($body);die();
         $this->assertEquals(1, $body['count']);
 
         $queryString = http_build_query(
