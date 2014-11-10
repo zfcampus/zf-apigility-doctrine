98b609f56c1c1d3295cc990910e234e887147b18
Gabriel Schmitt <gabrielsch@outlook.com>
2014-11-10 14:21:03 -0200

    Fixing some code standards
    Following PSR-1/2

--- src/Server/Paginator/Adapter/DoctrineOrmAdapter.php
+++ src/Server/Paginator/Adapter/DoctrineOrmAdapter.php
@@ -12,7 +12,11 @@ use Zend\Paginator\Adapter\AdapterInterface;
  */
 class DoctrineOrmAdapter extends Paginator implements AdapterInterface
 {
+    /**
+     * @var array
+    */
     public $cache = array();
+    
     /**
      * @param $offset
      * @param $itemCountPerPage
@@ -21,14 +25,19 @@ class DoctrineOrmAdapter extends Paginator implements AdapterInterface
      */
     public function getItems($offset, $itemCountPerPage)
     {
-        if(array_key_exists($offset, $this->cache)
-                && array_key_exists($itemCountPerPage, $this->cache[$offset]) )
+        if (array_key_exists($offset, $this->cache)
+            && array_key_exists($itemCountPerPage, $this->cache[$offset])
+        ) {
+            return $this->cache[$offset][$itemCountPerPage];    
+        }
 
-            return $this->cache[$offset][$itemCountPerPage];
         $this->getQuery()->setFirstResult($offset);
         $this->getQuery()->setMaxResults($itemCountPerPage);
 
-        if(!array_key_exists($offset, $this->cache)) $this->cache[$offset] = array();
+        if(! array_key_exists($offset, $this->cache)) {
+            $this->cache[$offset] = array();
+        }
+        
         $this->cache[$offset][$itemCountPerPage] =  $this->getQuery()->getResult();
 
         return $this->cache[$offset][$itemCountPerPage];
