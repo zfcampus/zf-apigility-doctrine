c9173aedc3bd7d6a3a1518e3c62b20c5c5e66b29
Tom Anderson <tom.h.anderson@gmail.com>
2014-11-09 18:52:10 -0700

    Removed old view info for server

--- config/server.config.php
+++ config/server.config.php
@@ -72,10 +72,4 @@ return array(
             ),
         ),
     ),
-
-    'view_manager' => array(
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
 );
