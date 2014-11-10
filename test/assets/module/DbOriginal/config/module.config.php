bf0d55f088b61e6ffd2f867847bf5094ea51e31a
Tom Anderson <tom.h.anderson@gmail.com>
2014-11-09 19:32:05 -0700

    clear template paths

--- test/assets/module/DbOriginal/config/module.config.php
+++ test/assets/module/DbOriginal/config/module.config.php
@@ -30,10 +30,6 @@ return [
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ),
         'strategies' => array(
             'ViewJsonStrategy',
