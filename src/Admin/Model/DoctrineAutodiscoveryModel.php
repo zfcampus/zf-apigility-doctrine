5e69d7a4d8c27303384c482392969d7aa0839da4
Daniel Schulz <takethefake@users.noreply.github.com>
2015-03-17 18:11:40 +0100

    Update DoctrineAutodiscoveryModel.php

--- src/Admin/Model/DoctrineAutodiscoveryModel.php
+++ src/Admin/Model/DoctrineAutodiscoveryModel.php
@@ -53,7 +53,7 @@ class DoctrineAutodiscoveryModel extends AbstractAutodiscoveryModel
                 switch ($mapping['type']) {
                     case 'string':
                         $field['filters'] = $this->filters['text'];
-                        if (isset($mapping['length'])) {
+                        if (isset($mapping['length']) && $mapping['length']) {
                             $validator = $this->validators['text'];
                             $validator['options']['max'] = $mapping['length'];
                             $field['validators'][] = $validator;
