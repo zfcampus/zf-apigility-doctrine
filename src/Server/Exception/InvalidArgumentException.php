b19ede230a8ff7478409524b9d47c3ec98b182c0
Phillip McCaffrey <pmccaffrey@synrgtech.com>
2015-05-09 18:49:07 -0700

    Added module-locale Exception interface and sanity check for setQueryProviders (ensuring injected queryProviders implement QueryProviderInterface)

---
+++ src/Server/Exception/InvalidArgumentException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ZF\Apigility\Doctrine\Server\Exception;
+
+/**
+ * Class InvalidArgumentException
+ *
+ * @package ZF\Apigility\Doctrine\Server\Exception
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+
+}
