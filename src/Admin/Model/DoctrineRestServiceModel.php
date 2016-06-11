e1f8ab7c0df2f80c04a297e3c76303a9bd1b6022
Tom H Anderson <tom.h.anderson@gmail.com>
2016-06-11 14:13:19 -0600

    Removed default, and missing, hydrator strategy

--- src/Admin/Model/DoctrineRestServiceModel.php
+++ src/Admin/Model/DoctrineRestServiceModel.php
@@ -768,23 +768,6 @@ class DoctrineRestServiceModel implements EventManagerAwareInterface, ServiceMan
         $objectManager      = $this->getServiceManager()->get($details->objectManager);
         $hydratorStrategies = array();
 
-        // Add all ORM collections to Hydrator Strategies
-        if ($objectManager instanceof \Doctrine\ORM\EntityManager) {
-            $collectionStrategyName = 'ZF\Apigility\Doctrine\Server\Hydrator\Strategy\CollectionLink';
-            $metadataFactory = $objectManager->getMetadataFactory();
-            $metadata = $metadataFactory->getMetadataFor($entityClass);
-
-            foreach ($metadata->associationMappings as $relationName => $relationMapping) {
-                switch ($relationMapping['type']) {
-                    case 4:
-                        $hydratorStrategies[$relationName] = $collectionStrategyName;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
         // The abstract_factories key is set to the value so these factories do not get duplicaed with each resource
         $config = array(
             'zf-apigility' => array(
