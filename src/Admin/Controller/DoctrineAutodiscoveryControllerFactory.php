73740e11730d1511393bdecfae28c8cd55e6bdf1
webimpress <contact@webimpress.com>
2016-08-02 22:37:25 +0100

    Updated DoctrineAutodiscoveryControllerFactory to be compatible with ZF2 and ZF3

--- src/Admin/Controller/DoctrineAutodiscoveryControllerFactory.php
+++ src/Admin/Controller/DoctrineAutodiscoveryControllerFactory.php
@@ -1,27 +1,49 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2015-2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZF\Apigility\Doctrine\Admin\Controller;
 
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\AbstractPluginManager;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use ZF\Apigility\Doctrine\Admin\Model\DoctrineAutodiscoveryModel;
 
 class DoctrineAutodiscoveryControllerFactory implements FactoryInterface
 {
     /**
-     * Create service
+     * Create and return DoctrineAutodiscoveryController instance.
      *
-     * @param ServiceLocatorInterface $controllers
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param null|array $options
      * @return DoctrineAutodiscoveryController
      */
-    public function createService(ServiceLocatorInterface $controllers)
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $services = $controllers->getServiceLocator();
-        /** @var \ZF\Apigility\Doctrine\Admin\Model\DoctrineAutodiscoveryModel $model */
-        $model = $services->get('ZF\Apigility\Doctrine\Admin\Model\DoctrineAutodiscoveryModel');
+        /** @var DoctrineAutodiscoveryModel $model */
+        $model = $container->get(DoctrineAutodiscoveryModel::class);
+
         return new DoctrineAutodiscoveryController($model);
+    }
+
+    /**
+     * Create and return DoctrineAutodiscoveryController instance (v2).
+     *
+     * Provided for backwards compatibility; proxies to __invoke().
+     *
+     * @param ServiceLocatorInterface $container
+     * @return DoctrineAutodiscoveryController
+     */
+    public function createService(ServiceLocatorInterface $container)
+    {
+        if ($container instanceof AbstractPluginManager) {
+            $container = $container->getServiceLocator() ?: $container;
+        }
+
+        return $this($container, DoctrineAutodiscoveryController::class);
     }
 }
